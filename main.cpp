309c687bcd266122e00792194c503627e0f3ab0e
unknown <kingdog10050@gmail.com>
2026-03-31 17:14:25 +0300

    Pridetas pradinis failas

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    const int MAX_STUDENTS = 100;
+    const int MAX_GRADES = 10;
+
+    string names[MAX_STUDENTS];            // Mokinų vardai
+    int grades[MAX_STUDENTS][MAX_GRADES];  // Jų pažymiai
+    int gradeCount[MAX_STUDENTS] = {0};    // Kiek pažymių turi kiekvienas mokinys
+    int studentCount = 0;
+
+    int choice;
+
+    return 0;
+}
